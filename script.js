590605c66a2fcd186cbf609e7f4da3fc02e78a1a
johnkimaiyo <kimaiyojohn6@gmail.com>
2022-08-11 13:33:53 +0300

    event listeners

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
+
+let activeLink = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    })
+})
